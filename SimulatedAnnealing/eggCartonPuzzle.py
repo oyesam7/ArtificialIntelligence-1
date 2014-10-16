cb382143c78c04ee4764cf72669c7e17b93819c0
Guo Junjun <guojunjun@gmail.com>
2014-10-16 22:46:40 +0200

    SimulatedAnnealing

---
+++ SimulatedAnnealing/eggCartonPuzzle.py
@@ -0,0 +1,16 @@
+from simulatedAnnealing import SA
+
+class Node:
+    M, N, K, board = None, None, None, None
+    def __init__(self,board):
+        self.board = board
+        M = len(board)
+        N = len(board[0])
+    def F(): #objectiveFunction():
+
+    def Gn(): #generate neighbors
+
+# M: rows; N: columns; K: (row, column or diagonal) no more than K eggs
+def eggCarton(M, N, K): 
+    carton = [[0]*M]*N
+
