d59a852a4bc30820035cc183b8036ea6ff6de0f2
Guo Junjun <guojunjun@gmail.com>
2014-10-17 21:48:38 +0200

    simulated annealing 01

--- SimulatedAnnealing/eggCartonPuzzle.py
+++ SimulatedAnnealing/eggCartonPuzzle.py
@@ -2,15 +2,32 @@ from simulatedAnnealing import SA
 
 class Node:
     M, N, K, board = None, None, None, None
-    def __init__(self,board):
+
+    def __init__(self, board, K):
         self.board = board
+        self.K = K
         M = len(board)
         N = len(board[0])
-    def F(): #objectiveFunction():
+        if (K > math.min(M, N)):
+            print
+            "Wrong K value!!!"
 
-    def Gn(): #generate neighbors
+    def F():                                    # objective Function return a evaluated quality in [0,1]:
 
-# M: rows; N: columns; K: (row, column or diagonal) no more than K eggs
-def eggCarton(M, N, K): 
-    carton = [[0]*M]*N
+                                                # horrible, mediocre, promising, excellent and optimal solutions...
+                                                # toward the optimal solution.
 
+    def Gn():                                   # generate neighbors
+
+                                                # M: rows; N: columns; K: (row, column or diagonal) no more than K eggs
+
+def eggCarton(M, N, K):
+    board = [[False] * M] * N                   # build and empty N*M carton
+    for k in range(K):
+        board[0][k] = True                      # start at fill K eggs in first row
+    state = Node(board, K)                      # build first state
+    Tmax, dT = 99, 1                            # Tmax max temperature; dT: reduce temperature size
+    Ftarget = math.min(M, N)
+    while (True):
+        solution = SA(state, Tmax, Ftarget, dT)
+        Ftarget = math.max(Ftarget, solution)
--- SimulatedAnnealing/simulatedAnnealing.py
+++ SimulatedAnnealing/simulatedAnnealing.py
@@ -1,8 +1,9 @@
 import math
+
 def SA(P, Tmax, Ftarget, dT):                   # 1.    start point P: start random or defined value
     T = Tmax                                    # 2.    set temperature to Tmax
     while (true):                               # 3.    objective function F evaluate samples
-        if P.F(P) >= Ftarget: return P          # 4.    return P as solution
+        if P.F(P) >= Ftarget or T<=0: return P  # 4.    return P as solution
         neighbors = P.Gn(P)                     # 5.    generate n neighbors of P
         for i in range(len(neighbors)):
             eneighbors[i] = P.F(neighbors[i])   # 6.    Evaluate each neighbor
